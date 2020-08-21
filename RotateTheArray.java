700d6933d636f337b96463fb230e5cb9b6f02529
Rajeev Kumar <rajeevkumar@Rajeevs-MacBook-Pro-2.local>
2020-08-21 17:55:50 +0530

    improved code of how to rotate sorted array k times

--- RotateTheArray.java
+++ RotateTheArray.java
@@ -7,11 +7,11 @@ public class RotateTheArray {
     public static void main(String[] str){
         int arr[] = {1,2,3,4,5,6};
         System.out.println("array before rotation" + Arrays.toString(arr));
-        rotateArrayBruteForce(arr,3);
+        rotateArrayBruteForce(arr,2);
         System.out.println("array after rotation (brute force)" + Arrays.toString(arr));
 
         int anotherArr[] = {1,2,3,4,5,6};
-        rotateArrayOptimalSolution(anotherArr,3);
+        rotateArrayOptimalSolution(anotherArr,1);
         System.out.println("array after rotation (optimal solution)" + Arrays.toString(anotherArr));
     }
     /*
@@ -38,34 +38,27 @@ public class RotateTheArray {
     Space complexity: O(1)
      */
     public static void rotateArrayOptimalSolution(int[] arr,  int k){
-        int i = 0;
-        int j = arr.length-k-1;
-        while(i<j){
-            swap(arr,i,j);
-            i++;
-            j--;
-        }
-        i = arr.length-k;
-        j = arr.length-1;
-        while(i<j){
-            swap(arr,i,j);
-            i++;
-            j--;
-        }
+        // reverse first n-k element
+        reverseFun(arr, 0, arr.length-k-1);
 
-        // now reverse the entire array
-        i = 0;
-        j = arr.length-1;
-        while(i<j){
-            swap(arr,i,j);
-            i++;
-            j--;
-        }
+        // reverse last k element
+        reverseFun(arr, arr.length-k, arr.length-1);
+
+        // reverse entire array
+        reverseFun(arr, 0, arr.length-1);
     }
 
     public static void swap(int[] arr, int x, int y){
         arr[x] = arr[x] + arr[y];
         arr[y] = arr[x] - arr[y];
         arr[x] = arr[x] - arr[y];
+    }
+
+    public static void reverseFun(int[] a, int left, int right){
+        while(left<right){
+            swap(a,left,right);
+            left++;
+            right--;
+        }
     }
 }
